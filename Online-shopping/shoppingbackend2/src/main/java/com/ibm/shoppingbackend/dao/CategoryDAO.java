4b50eec082db37556e99def3938f78f6d1ed2d36
Karanveer Singh <skaranveer712@gmail.com>
2021-07-09 14:03:58 +0800

    final project

---
+++ Online-shopping/shoppingbackend2/src/main/java/com/ibm/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,18 @@
+package com.ibm.shoppingbackend.dao;
+
+import java.util.List;
+
+import com.ibm.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	
+	
+	Category get(int id);
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+	
+}
